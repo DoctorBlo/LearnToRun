c56dfe10700a996424a2ebf3f2585589182bea3f
Polina Zablotskaia <polinazablotskaia@Polinas-MBP.hitronhub.home>
2017-12-14 05:09:11 -0800

    the right OUprocess

--- DDPG/DDPG.py
+++ DDPG/DDPG.py
@@ -27,6 +27,8 @@ class DDPG:
         #more a dimensionality thing
         self.state = s
         self.action = a
+        self.OUarray = self.OUprocess(0.2, 0.15, 0.0)
+        self.step = 1
 
     def processNoise(self):
         #this should be something more eloquent....
@@ -41,36 +43,32 @@ class DDPG:
                 ret[i] = -ret[i]
         return ret
 
-    def OUprocess(self, sigma, theta):
+    def OUprocess(self, sigma, theta, mu):
         # define model parameters
         t_0 = 0
         t_end = 2
-        length = self.action
-        mu = 0
+        length = 1000
 
+        y = np.zeros((length, self.action),dtype="f")
         t = np.linspace(t_0,t_end,length) # define time axis
         dt = np.mean(np.diff(t))
-        y = np.zeros(length)
         y0 = np.random.normal(loc=0.0,scale=1.0) # initial condition
         drift = lambda y,t: theta*(mu-y) # define drift term
         diffusion = lambda y,t: sigma # define diffusion term
-        noise = np.random.normal(loc=0.0,scale=1.0,size=length)*np.sqrt(dt) #define noise process
-        # solve SDE
-        for i in xrange(1,length):
-            y[i] = y[i-1] + drift(y[i-1],i*dt)*dt + diffusion(y[i-1],i*dt)*noise[i]
 
-        ret = torch.zeros(self.action)
-        for i in range(0, self.action):
-            ret[i] = y[i]
-        return ret
+        # solve SDE
+        for j in xrange(1, self.action):
+            noise = np.random.normal(loc=0.0,scale=1.0,size=length)*np.sqrt(dt) #define noise process
+            for i in xrange(1,length):
+                y[i][j] = y[i-1][j] + drift(y[i-1][j],i*dt)*dt + diffusion(y[i-1][j],i*dt)*noise[i]
+        return y
 
     def selectAction(self, state):
         #remember, state better be an autograd Variable
         ret = self.targetActor(Variable(state)).data
         if(random.random() > .9):
-            ret = ret + self.OUprocess(0.2, 0.15)
-
-
+            ret = ret + torch.from_numpy(self.OUarray[self.step])
+        self.step += 1
         return torch.clamp(ret, 0.0, 1.0)
 
     def addToMemory(self, state, action, reward, stateprime):
